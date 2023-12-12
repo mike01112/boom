1e846a32277f0d14070eea9161c8316da2f28cea
Michael Hagos <michaelhagos163@gmail.com>
2023-12-12 18:38:00 +0300

    chrisbrown

---
+++ models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
